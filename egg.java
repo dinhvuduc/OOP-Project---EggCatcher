3581c61a69c5e5bc5610289bb071ac9c877b2a7d
nhathuy19135 <74639606+nhathuy19135@users.noreply.github.com>
2020-11-19 13:09:53 +0700

    Update egg.java

--- egg.java
+++ egg.java
@@ -12,6 +12,6 @@ import java.util.Random;
 import javax.swing.*;
 import javax.swing.UIManager.LookAndFeelInfo;
 import javax.swing.border.TitledBorder;
-public class egg{
+public class egg extends Jpanel implements Runnable {
     
 }
