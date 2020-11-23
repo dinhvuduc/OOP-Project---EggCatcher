82bb53af88d3e309ceb6654ac252cfe5e16f25ef
nhathuy19135 <74639606+nhathuy19135@users.noreply.github.com>
2020-11-23 08:13:29 +0700

    Update egg.java

--- egg.java
+++ egg.java
@@ -5,5 +5,23 @@ import javax.swing.*;
 import javax.swing.UIManager.LookAndFeelInfo;
 import javax.swing.border.TitledBorder;
 public class egg extends Jpanel implements Runnable {
-    
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+    Thread th;
+    Graphics2D g;
+    int dx,dy;
+    Bowl b= new Bowl();
+    int NUM=5;
+    Egg egg[]= new Egg[NUM];
+    EggMover movers[]= new EggMover[NUM];
+    boolean inside=false;
+    Timer timer[] = new Timer[NUM];
+    static int FWIDTH=500,FHEIGHT=400;
+    int in=0;
+    int speed=15;
+    int lifes=10;
+    static Color c1=Color.ORANGE,c2=Color.YELLOW;
+   
 }
