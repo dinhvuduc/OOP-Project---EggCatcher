fc9863944142747faf47c04f29b1e5620e860f71
dinhvuduc <dinhvuduc751@gmail.com>
2020-11-24 23:42:22 +0700

    added aint method

--- egg.java
+++ egg.java
@@ -89,3 +89,61 @@ public class egg extends Jpanel implements Runnable {
     
 
 }
+public void paint(Graphics g2){
+    g=(Graphics2D)g2;
+    g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+    super.paint(g);
+    GradientPaint gpp = new GradientPaint(getWidth()/2,0,c1,getWidth()/2,getHeight(),c2);
+    g.setPaint(gpp);
+    g.fillRect(0, 0, getWidth(), getHeight());
+    b.setMy(getHeight()-35);
+    Polygon poly = new Polygon();
+    poly.addPoint((getWidth()/3)/2,getHeight()-40);
+    poly.addPoint(getWidth()-(getWidth()/3)/2, getHeight()-40);
+    poly.addPoint(getWidth()-10, getHeight()-10);
+    poly.addPoint(10, getHeight()-10);
+    GradientPaint gp = new GradientPaint(getWidth()/2,getHeight(),Color.WHITE,getWidth()/2,getHeight()-40,Color.DARK_GRAY);
+    g.setPaint(gp);
+    g.fill(poly);
+    b.drawOn(g);    
+    for(int i=0;i<egg.length;++i){
+                egg[i].drawOn(g);
+    }
+    }
+    
+    
+    
+    
+    public void run(){
+        try{
+            while(true){
+                th.sleep(speed);
+                if(b.lifes==0){
+                    for(int i=0;i<egg.length;++i){
+                                egg[i].reset();
+                                movers[i].stop();
+                            }
+                            JOptionPane.showMessageDialog(null, "Your Score Was "+b.score);
+                            int a=JOptionPane.showConfirmDialog(null, "Do You Want To Restart The Game", "Game Over", JOptionPane.OK_CANCEL_OPTION);
+                            
+                        if(a==JOptionPane.OK_OPTION){
+                            for(int i=0;i<egg.length;++i){
+                               movers[i].setInitialDelay((i+1)*1500);
+                                movers[i].move();
+                            }
+                            b.score=-10;
+                            b.lifes=10;
+                            b.updateScore();
+                            
+                        }
+                        if(a==JOptionPane.CANCEL_OPTION){
+                            System.exit(0);
+                        }
+                        }
+                repaint();
+            }
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+    
