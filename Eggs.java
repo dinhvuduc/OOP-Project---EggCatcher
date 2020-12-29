c967dead91a0bc980eb6d6a53e67bb66497483ca
nhathuy19135 <74639606+nhathuy19135@users.noreply.github.com>
2020-12-29 20:33:41 +0700

    Update Eggs.java

--- Eggs.java
+++ Eggs.java
@@ -41,14 +41,12 @@ public class Eggs extends JPanel implements Runnable {
                     break;
                 }
             }
-        }
-
-        catch(Exception e){
+        }catch(Exception e){
             System.out.println(e);
         }
         //Listener Starts//
 
-        for(int i = 0; i < egg.length; i++ ){
+        for(int i = 0;i<egg.length;++i ){
             egg[i]  =   new Egg();
             egg[i].setMy(30);
             egg[i].reset();
@@ -225,49 +223,7 @@ class EggMover{
     void stop(){
         timer.stop();
     }
-    class Bowl{
-
-        int MX,MY;
-        Rectangle r;
-        JLabel l;
-        int score=0,lifes=10;
-        Bowl(){
-        }
-        
-        void setMx(int dx){
-            MX=dx;
-        }
-        
-        void setOutputComponent(JLabel lb){
-            l=lb;
-        }
-        
-        void updateScore(){
-          l.setText("Score = "+ (score+=10)+"          Lifes = "+(lifes));
-        }
-        
-        void updateLife(){
-            l.setText("Score = "+ (score+=10)+"          Lifes = "+(lifes));
-        }
-        
-        void setMy(int dy){
-            MY=dy;
-        }
-        
-        boolean contains(Point p){
-            return r.contains(p);
-        }
-        
-        void drawOn(Graphics2D g){
-            GradientPaint gp1= new GradientPaint(MX-15,MY-10,Color.LIGHT_GRAY,MX-15+30/2,MY-10+30/2,Color.WHITE,true);
-            g.setPaint(gp1);
-            g.fillArc(MX-15, MY-10, 30, 30, 0, -180);
-            GradientPaint gp2= new GradientPaint(MX,MY,Color.WHITE,MX,MY+10,Color.DARK_GRAY.brighter());
-            g.setPaint(gp2);
-            g.fillOval(MX-15, MY, 30, 10);
-            r=new Rectangle(MX-15,MY,30,20);
-        }
-    }
+    
 }
 class Bowl{
 
